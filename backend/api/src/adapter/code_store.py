e4eb248442752ca7e37144b361a8d77ae896475f
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2022-05-29 18:38:00 +0200

    Insert data in code store

--- backend/api/src/adapter/code_store.py
+++ backend/api/src/adapter/code_store.py
@@ -1,30 +1,26 @@
 import boto3
+from datetime import datetime, timezone
 from port.port import CodeStore
 
 
 class DynamoCodeStore(CodeStore):
 
-
     def __init__(self, dynamo_table_name):
         self.table_name = dynamo_table_name
         self.client = boto3.client("dynamodb")
 
-
     def register_code(self, meet_id):
-        # TODO Add generation date ?
-        # TODO Add used / not used
+        now = datetime.now(timezone.utc).isoformat()
         self.client.put_item(
             TableName=self.table_name,
-            Item={'meet_id': {'S': meet_id}})
-
+            Item={'meet_id': {'S': meet_id}, 'creation_date': {'S': now}})
 
     def code_exists(self, meet_id) -> bool:
         result = self.client.get_item(
             TableName=self.table_name,
-            Key={'meet_id':{'S':meet_id}}
+            Key={'meet_id': {'S': meet_id}}
         )
         return "Item" in result
-
 
     def count_codes(self) -> int:
         dynamodb = boto3.resource('dynamodb')
