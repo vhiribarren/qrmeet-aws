2082b6ed6a313499465a1d2acf88587d2f0d53d3
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2022-05-31 21:02:30 +0200

    Enhance a test case on score when meeting people

--- backend/api/test/test_meet_event.py
+++ backend/api/test/test_meet_event.py
@@ -77,10 +77,14 @@ def test_api_meet_increase_half_score_and_mutual_meet_increase_overall_score_and
                                                                                                      ranking_service):
     user_1 = new_user("user1")
     user_2 = new_user("user2")
+    user_3 = new_user("user3")
     score_1 = ranking_service.user_ranking(user_1.phone_id)
     score_2 = ranking_service.user_ranking(user_2.phone_id)
+    score_3 = ranking_service.user_ranking(user_3.phone_id)
     assert score_1.score_full == 0 and score_1.score_half == 0
     assert score_2.score_full == 0 and score_2.score_half == 0
+    assert score_3.score_full == 0 and score_3.score_half == 0
+
     meet_event = {
         "encounter_meet_id": user_2.meet_id,
         "from_phone_id": user_1.phone_id,
@@ -88,8 +92,23 @@ def test_api_meet_increase_half_score_and_mutual_meet_increase_overall_score_and
     api_post(API_MEET, json.dumps(meet_event))
     score_1 = ranking_service.user_ranking(user_1.phone_id)
     score_2 = ranking_service.user_ranking(user_2.phone_id)
+    score_3 = ranking_service.user_ranking(user_3.phone_id)
     assert score_1.score_full == 0 and score_1.score_half == 1
     assert score_2.score_full == 0 and score_2.score_half == 0
+    assert score_3.score_full == 0 and score_3.score_half == 0
+
+    meet_event = {
+        "encounter_meet_id": user_3.meet_id,
+        "from_phone_id": user_1.phone_id,
+    }
+    api_post(API_MEET, json.dumps(meet_event))
+    score_1 = ranking_service.user_ranking(user_1.phone_id)
+    score_2 = ranking_service.user_ranking(user_2.phone_id)
+    score_3 = ranking_service.user_ranking(user_3.phone_id)
+    assert score_1.score_full == 0 and score_1.score_half == 2
+    assert score_2.score_full == 0 and score_2.score_half == 0
+    assert score_3.score_full == 0 and score_3.score_half == 0
+
     meet_event = {
         "encounter_meet_id": user_1.meet_id,
         "from_phone_id": user_2.phone_id,
@@ -97,5 +116,7 @@ def test_api_meet_increase_half_score_and_mutual_meet_increase_overall_score_and
     api_post(API_MEET, json.dumps(meet_event))
     score_1 = ranking_service.user_ranking(user_1.phone_id)
     score_2 = ranking_service.user_ranking(user_2.phone_id)
-    assert score_1.score_full == 1 and score_1.score_half == 0
+    score_3 = ranking_service.user_ranking(user_3.phone_id)
+    assert score_1.score_full == 1 and score_1.score_half == 1
     assert score_2.score_full == 1 and score_2.score_half == 0
+    assert score_3.score_full == 0 and score_3.score_half == 0
